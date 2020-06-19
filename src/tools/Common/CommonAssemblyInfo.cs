ee23fef59fb9457b4387bb29827dfb574798f69c
Michael W. Powell <mwpowelllde@gmail.com>
2020-06-19 18:57:09 -0400

    added common assembly info which is transitioning into the directory build properties

---
+++ src/tools/Common/CommonAssemblyInfo.cs
@@ -0,0 +1,8 @@
+//using System.Reflection;
+
+//[assembly: AssemblyConfiguration("")]
+//[assembly: AssemblyCompany("Ellumination Technologies")]
+//[assembly: AssemblyProduct("Ellumination.Collections")]
+//[assembly: AssemblyCopyright("Copyright © 2016-2020")]
+//[assembly: AssemblyTrademark("")]
+//[assembly: AssemblyCulture("")]
