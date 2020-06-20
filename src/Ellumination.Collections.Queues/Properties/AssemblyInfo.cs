6ba76d1ee03096bcf19b56e5eb2c937dd64c8e8a
Michael W. Powell <mwpowelllde@gmail.com>
2020-06-20 17:26:45 -0400

    commented out unnecessary assembly info

--- src/Ellumination.Collections.Queues/Properties/AssemblyInfo.cs
+++ src/Ellumination.Collections.Queues/Properties/AssemblyInfo.cs
@@ -1,9 +1,9 @@
-﻿using System.Runtime.InteropServices;
+﻿//using System.Runtime.InteropServices;
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+//// Setting ComVisible to false makes the types in this assembly not visible 
+//// to COM components.  If you need to access a type in this assembly from 
+//// COM, set the ComVisible attribute to true on that type.
+//[assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("f0dd2e97-32c8-4281-8abb-138dee4b9984")]
+//// The following GUID is for the ID of the typelib if this project is exposed to COM
+//[assembly: Guid("f0dd2e97-32c8-4281-8abb-138dee4b9984")]
