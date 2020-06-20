6a71780fdb17620abfcd0841a2427fb2bd6cc776
Michael W. Powell <mwpowelllde@gmail.com>
2020-06-20 15:51:34 -0400

    commented out unnecessary assembly info

--- src/Ellumination.Collections.Enumerations.Tests/Properties/AssemblyInfo.cs
+++ src/Ellumination.Collections.Enumerations.Tests/Properties/AssemblyInfo.cs
@@ -1,6 +1,6 @@
-using System.Runtime.InteropServices;
+//using System.Runtime.InteropServices;
 
-[assembly: ComVisible(false)]
+//[assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("3557d09f-2fed-4535-a974-7a26042d3174")]
+//// The following GUID is for the ID of the typelib if this project is exposed to COM
+//[assembly: Guid("3557d09f-2fed-4535-a974-7a26042d3174")]
