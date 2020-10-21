79a5c9d2ec9af83a1d381460cc7c00c9765ca1ab
Michael W. Powell <mwpowelllde@gmail.com>
2020-10-21 14:00:37 -0400

    Added Singleton assets.

---
+++ src/Ellumination.Collections.Bidirectionals/Generic/BidirectionalSingleton.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Ellumination.Collections.Generic
+{
+    /// <inheritdoc cref="IBidirectionalSingleton"/>
+    public abstract class BidirectionalSingleton : IBidirectionalSingleton
+    {
+        /// <inheritdoc/>
+        public Type ValueType { get; }
+
+        /// <inheritdoc/>
+        public Type SingletonType { get; }
+
+        /// <inheritdoc/>
+        public bool IsReadOnly { get; set; }
+
+        /// <summary>
+        /// Gets the UnboundSingletonType.
+        /// </summary>
+        public static Type UnboundSingletonType { get; } = typeof(BidirectionalSingleton<>);
+
+        /// <summary>
+        /// Protected Internal Constructor.
+        /// </summary>
+        /// <param name="valueType"></param>
+        protected internal BidirectionalSingleton(Type valueType)
+        {
+            this.ValueType = valueType;
+            this.SingletonType = UnboundSingletonType.MakeGenericType(valueType);
+        }
+    }
+}
---
+++ src/Ellumination.Collections.Bidirectionals/Generic/IBidirectionalSingleton.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Ellumination.Collections.Generic
+{
+    /// <summary>
+    /// Provides the basic Singleton interface.
+    /// </summary>
+    public interface IBidirectionalSingleton
+    {
+        /// <summary>
+        /// Gets the ValueType.
+        /// </summary>
+        Type ValueType { get; }
+
+        /// <summary>
+        /// Gets the SingletonType.
+        /// </summary>
+        Type SingletonType { get; }
+
+        /// <summary>
+        /// Gets or Sets whether the Singleton IsReadOnly.
+        /// </summary>
+        bool IsReadOnly { get; set; }
+    }
+}
