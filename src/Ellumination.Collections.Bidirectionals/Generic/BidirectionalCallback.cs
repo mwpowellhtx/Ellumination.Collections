dc7bab40f58c342c299f2ee610b06df2cfbdfd42
Michael W. Powell <mwpowelllde@gmail.com>
2020-10-21 17:30:09 -0400

    Updated callbacks after some unit testing.

--- src/Ellumination.Collections.Bidirectionals/Generic/BidirectionalCallback.cs
+++ src/Ellumination.Collections.Bidirectionals/Generic/BidirectionalCallback.cs
@@ -32,14 +32,15 @@
     /// <typeparam name="T"></typeparam>
     /// <param name="old"></param>
     /// <param name="current"></param>
-    public delegate void BidirectionalSingletonOnClause<in T>(T old, T current);
+    /// <param name="value"></param>
+    public delegate void BidirectionalSingletonOnClause<in T>(T old, T current, T value);
 
     /// <summary>
     /// Callback occurs approaching <see cref="BidirectionalSingleton{T}.Value"/> Set clause.
     /// </summary>
     /// <typeparam name="T"></typeparam>
     /// <param name="old"></param>
-    /// <param name="current"></param>
+    /// <param name="value"></param>
     /// <returns></returns>
-    public delegate bool TryBidirectionalSingletonOnSetClause<in T>(T old, T current);
+    public delegate bool TryBidirectionalSingletonOnSetClause<in T>(T old, T value);
 }
