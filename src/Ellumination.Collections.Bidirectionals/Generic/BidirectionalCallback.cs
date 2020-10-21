b42ce48bf4281de72f6bfab69edbf3020129ceb8
Michael W. Powell <mwpowelllde@gmail.com>
2020-10-21 14:09:19 -0400

    Added Singleton delegates.

--- src/Ellumination.Collections.Bidirectionals/Generic/BidirectionalCallback.cs
+++ src/Ellumination.Collections.Bidirectionals/Generic/BidirectionalCallback.cs
@@ -15,4 +15,31 @@
     /// <param name="key"></param>
     /// <param name="value"></param>
     public delegate void BidirectionalDictionaryKeyValueCallback<in TKey, in TValue>(TKey key, TValue value);
+
+    /// <summary>
+    /// Callback evaluates approaching <see cref="BidirectionalSingleton{T}.Value"/> Get clause.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="old"></param>
+    /// <param name="result"></param>
+    /// <returns></returns>
+    public delegate bool TryBidirectionalSingletonOnGetClause<T>(T old, out T result);
+
+    /// <summary>
+    /// Callback occurs at strategic moments during <see cref="BidirectionalSingleton{T}.Value"/>
+    /// Set or Get clauses.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="old"></param>
+    /// <param name="current"></param>
+    public delegate void BidirectionalSingletonOnClause<in T>(T old, T current);
+
+    /// <summary>
+    /// Callback occurs approaching <see cref="BidirectionalSingleton{T}.Value"/> Set clause.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="old"></param>
+    /// <param name="current"></param>
+    /// <returns></returns>
+    public delegate bool TryBidirectionalSingletonOnSetClause<in T>(T old, T current);
 }
